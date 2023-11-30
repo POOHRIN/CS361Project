f133be55b8e9649f6e9b2a699ac9ced38cb48df8
HaySad <beemxxza@gmail.com>
2023-12-01 02:54:48 +0700

    อัพของ @methavee11
    แล้วก็เพิ่มหัวด้านล่างและบนของ @methavee11 เข้าไปในของเราด้วยด้วย
    
    Co-Authored-By: methavee11 <151167712+methavee11@users.noreply.github.com>

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -11,4 +11,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    protected void onCreate(){
+        ImageButton myImageView = findViewById(R.id.imageButton);
+        // Get current dimensions
+        int width = myImageView.getLayoutParams().width;
+        int height = myImageView.getLayoutParams().height;
+        // Double the dimensions
+        myImageView.getLayoutParams().width = 2 * width;
+        myImageView.getLayoutParams().height = 2 * height;
+        // Apply the new dimensions
+        myImageView.requestLayout();
+    }
 }
