2497df294c80deca3fb0bf8a808798a1b1f56062
POOHRIN <147570157+POOHRIN@users.noreply.github.com>
2023-12-01 12:16:23 +0700

    update before present

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,15 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button record = findViewById(R.id.incomeButton);
-        Button summary = findViewById(R.id.summaryButton);
+        Button income = (Button) findViewById(R.id.incomeButton);
+        Button expense = (Button) findViewById(R.id.expenseButton);
+        ImageButton summary = (ImageButton) findViewById(R.id.graph);
+        ImageButton history = (ImageButton) findViewById(R.id.history);
 
-        Button record = (Button) findViewById(R.id.incomeButton);
-        Button summary = (Button) findViewById(R.id.summaryButton);
-        TextView history = (TextView) findViewById(R.id.historyButton);
-
-        record.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Record.class)));
-        summary.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Summary.class)));
+        income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Record.class));
+            }
+        });
+        expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Record.class));
+            }
+        });
         summary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,16 +47,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-    protected void onCreate(){
-        ImageButton myImageView = findViewById(R.id.imageButton);
-        // Get current dimensions
-        int width = myImageView.getLayoutParams().width;
-        int height = myImageView.getLayoutParams().height;
-        // Double the dimensions
-        myImageView.getLayoutParams().width = 2 * width;
-        myImageView.getLayoutParams().height = 2 * height;
-        // Apply the new dimensions
-        myImageView.requestLayout();
     }
 }
