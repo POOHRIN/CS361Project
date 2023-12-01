7a9198b1ecb264592776099d9c2167f0ef864f6c
Apisara Thepsing <apisara.the@dome.tu.ac.th>
2023-12-01 16:24:13 +0700

    สวยนะ เชื่อสิ

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+
+        super.onConfigurationChanged(newConfig);
+        final TextView textView = findViewById(R.id.textView);
+        textView.setTextSize(newConfig.fontScale*32);
+        Toast.makeText(getApplicationContext(),newConfig.fontScale+"",Toast.LENGTH_LONG).show();
     }
 }
