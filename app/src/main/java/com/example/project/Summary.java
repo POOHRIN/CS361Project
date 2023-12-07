4b4853f6533624c2d1576310e9b45d3921ba727d
POOHRIN <147570157+POOHRIN@users.noreply.github.com>
2023-12-07 22:03:43 +0700

    update summary

--- app/src/main/java/com/example/project/Summary.java
+++ app/src/main/java/com/example/project/Summary.java
@@ -1,25 +1,22 @@
 package com.example.project;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class Summary extends AppCompatActivity {
 
@@ -51,7 +48,7 @@ public class Summary extends AppCompatActivity {
 
         // initializing variable for bar chart.
         barChart = findViewById(R.id.BarChart);
-        Button show = (Button) findViewById(R.id.showButton);
+        Button chooseMonth = (Button) findViewById(R.id.chooseMonthbtn);
 
         TextView result = (TextView) findViewById(R.id.result);
 
@@ -79,61 +76,11 @@ public class Summary extends AppCompatActivity {
         barDataSet.setValueTextSize(16f);
         barChart.getDescription().setEnabled(false);
 
-        show.setOnClickListener(new View.OnClickListener() {
+        chooseMonth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText monthET = (EditText) findViewById(R.id.monthInput);
-                EditText yearET = (EditText) findViewById(R.id.yearInput);
-                int month = Integer.parseInt(String.valueOf(monthET.getText()));
-                int year = Integer.parseInt(String.valueOf(yearET.getText()));
 
-                if (month < 1 || month > 12 || year < 1990 || year > 2060) {
-                    if (!shown_dialog) {
-                        shown_dialog = true;
-                        final AlertDialog.Builder viewDialog;
-                        viewDialog = new AlertDialog.Builder(Summary.this);
-                        viewDialog.setTitle("Alert");
-                        viewDialog.setMessage("Enter the correct month and year format" +
-                                "\n enter year between 1990 to 2060");
-                        viewDialog.setPositiveButton("OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                        shown_dialog = false;
-                                    }
-                                });
-                        viewDialog.show();
-                    }
-                } else {
-                    result.setText("Month " +month + " Year " + year);
-                    income = 20000;
-                    expense = 18250;
-
-                    getBarEntries(income, expense);
-
-                    // creating a new bar data set.
-                    barDataSet = new BarDataSet(barEntriesArrayList, "Income & Expense");
-
-                    // creating a new bar data and
-                    // passing our bar data set.
-                    barData = new BarData(barDataSet);
-
-                    // below line is to set data
-                    // to our bar chart.
-                    barChart.setData(barData);
-
-                    // adding color to our bar data set.
-                    barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
-                    // setting text color.
-                    barDataSet.setValueTextColor(Color.BLACK);
-
-                    // setting text size
-                    barDataSet.setValueTextSize(16f);
-                    barChart.getDescription().setEnabled(false);
-
-                    barChart.invalidate();
-                }
+                barChart.invalidate();
             }
         });
 
