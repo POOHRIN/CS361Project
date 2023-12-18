d6663a855af6b12202039da14f4388f583aa6b1e
HaySad <beemxxza@gmail.com>
2023-12-18 14:06:48 +0700

    Update submit
    อัปนิดๆ เท่าที่ทำได้ เหลือ DB หรือการนำค่าไปเก็บข้อมูล
    
    เอาไว้ค่อยมาคุยต่อ
    
    @POOHRIN ฝากตรวจสอบแล้วนำขึ้น main ด้วย

--- app/src/main/java/com/example/project/Record.java
+++ app/src/main/java/com/example/project/Record.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import java.util.Calendar;
+
 
 public class Record extends AppCompatActivity {
 
@@ -23,7 +30,15 @@ public class Record extends AppCompatActivity {
         ImageButton history = (ImageButton) findViewById(R.id.history);
         ImageButton home = (ImageButton)findViewById(R.id.home);
         ImageButton back = (ImageButton)findViewById(R.id.back);
+        setContentView(R.layout.activity_record);
 
+        Button btnOpenDatePicker = findViewById(R.id.btnOpenDatePicker);
+        btnOpenDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
         a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,5 +72,59 @@ public class Record extends AppCompatActivity {
                 startActivity(new Intent(Record.this, MainActivity.class));
             }
         });
+
+        Button btnSave = findViewById(R.id.btnConfirm);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView dateTextView = findViewById(R.id.textView4);
+                String selectedDate = dateTextView.getText().toString();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Record.this);
+                builder.setTitle("ข้อมูลทีใส่");
+                builder.setMessage("วันที่ที่คุณเลือกคือ: " + selectedDate);
+                builder.setNegativeButton("ยกเลิก", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO: เดี๋ยวให้มาใส่ DB ในนี้
+                    }
+                });
+
+                builder.show();
+            }
+        });
     }
+
+
+    public void showDatePickerDialog(View view) {
+        // รับเวลาปัจจุบัน
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        if (view.isShown()) {
+                            String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+
+                            TextView date = findViewById(R.id.textView4);
+                            date.setText(selectedDate);
+                        }
+                    }
+                },
+                year, month, day);
+
+        datePickerDialog.show();
+    }
+
 }
